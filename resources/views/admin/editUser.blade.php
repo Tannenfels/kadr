b5974ae8cda10004eebdb98f56e604bb92bc0dc9
Tannenfels <tannenfels@yandex.ru>
2019-09-28 04:33:29 +0300

    CRUD

--- resources/views/admin/editUser.blade.php
+++ resources/views/admin/editUser.blade.php
@@ -41,7 +41,6 @@
                     <textarea class="form-control" style="height:150px" name="detail" placeholder="Detail">{{ $product->detail }}</textarea>
                 </div>
             </div>
-
             <div class="col-xs-12 col-sm-12 col-md-12 text-center">
                 <button type="submit" class="btn btn-primary">Submit</button>
             </div>
